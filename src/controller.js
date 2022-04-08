86b2c58531eac158894c594d26a8a39d1600ec01
amandbest786 <amandbest786@gmail.com>
2022-04-08 10:45:07 +0530

    project 4

--- src/controller.js
+++ src/controller.js
@@ -69,6 +69,7 @@ const createShortUrl = async (req, res) => {
 
       let savedData = await urlModel.create(urlCreated);
       await SET_ASYNC(`${urlCode}`, JSON.stringify(longUrl))
+      await SET_ASYNC(`${longUrl}`, JSON.stringify(longUrl))
       return res
         .status(201)
         .json({ status: true, msg: "Url Details.", data: savedData });
